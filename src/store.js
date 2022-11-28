4fc3c9a5d1010469ded344d42ce83c2b71a62fe9
damian.mosinski@vobacom.pl <damian.mosinski@vobacom.pl>
2022-11-28 17:06:54 +0100

    dodanie store zustand

---
+++ src/store.js
@@ -0,0 +1,23 @@
+import create from 'zustand'
+import { devtools, persist } from 'zustand/middleware'
+
+let tokenStore = (set) => ({
+  token: "",
+  setToken: ( token ) => set((state) => state.token = token),
+  removeToken: () => set((state) => state.token = ""),
+})
+
+let audiobookListStore = (set) => ({
+  people: [],
+  addPerson: (person) =>
+    set((state) => ({ people: [...state.people, person] })),
+//todo tu jeszcze inne metody
+})
+
+tokenStore = devtools(tokenStore)
+tokenStore = persist(tokenStore, { name: 'auth_token' })
+
+audiobookListStore = devtools(audiobookListStore)
+
+export const useTokenStore = create(tokenStore)
+export const useAudiobookListStore = create(audiobookListStore)
