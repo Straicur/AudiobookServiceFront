eda7f368bc2641e99dc892ebbb0bf5aaf66c46d4
damian.mosinski@vobacom.pl <mosinskidamian11@gmail.com>
2022-12-02 16:07:49 +0100

    Praca nad połączeniem

--- src/User/Login/UserLogin.js
+++ src/User/Login/UserLogin.js
@@ -1,8 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { HandleFetch } from '../../components/HandleFetch';
 
 export default function UserLogin() {
-    return (
-      <div className="App">
-        UserLogin
-      </div>
-    );
+
+  const handleLogin = () => {
+
+    const url = 'http://127.0.0.1:8000/api/authorize';
+    const method = 'POST';
+    const jsonData = { email: "admin@audio.com", password: "zaq12wsx" };
+
+    HandleFetch(url, jsonData, method)
+      // .then(data => data.json())
+      // .then(data => {
+      //   console.log(data);
+      // })
+      // .catch(e => {
+      //   console.log(e);
+      // });
+  }
+
+  useEffect( ()=>{
+    handleLogin();
+  } , []  )
+
+  return (
+    <div className="App">
+
+    </div>
+  );
 }
--- src/components/HandleFetch.js
+++ src/components/HandleFetch.js
@@ -1,7 +1,24 @@
 
-export default function HandleFetch() {
-    return (
-      <div className="App">
-      </div>
-    );
-}
+import {useState, useEffect} from 'react';
+
+export const HandleFetch = (url,jsonData,method) => {
+  const [state, setState] = useState("");
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (await fetch(url,
+        {
+          method:method,
+          body: JSON.stringify(jsonData)
+        }
+        )).json();
+
+        //todo tu zostaje obsałużenie do errora
+      setState(data);
+    };
+
+    dataFetch();
+  }, [url]);
+
+  return { data: state };
+};
