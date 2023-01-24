f7b6d7dfa3a9579ed786d81ca3c2f49c37948e8f
damian.mosinski@vobacom.pl <mosinskidamian11@gmail.com>
2023-01-24 17:40:51 +0100

    Dalej

--- src/Admin/Categories/Categories.js
+++ src/Admin/Categories/Categories.js
@@ -3,55 +3,28 @@ import { useTokenStore } from "../../store";
 import { ErrorBoundary } from "react-error-boundary";
 import { ErrorHandlerModal } from "../../Errors/ErrorHandlerModal";
 import CategoriesList from "./components/CategoriesList";
-import { useQuery } from "react-query";
-import { HandleFetch } from "../../components/HandleFetch";
-
 
 export default function Categories() {
   const token = useTokenStore((state) => state.token);
 
-  const [infoState, setInfoState] = useState({
-    users: 0,
-    categories: 0,
-    audiobooks: 0,
-    lastWeekRegistered: 0,
-    lastWeekLogins: 0,
-    lastWeekNotifications: 0,
+  const [categoiesState, setCategoiesState] = useState({
+    categories: [],
     error: null,
   });
-  const { isLoading, error, data, isFetching, refetch } = useQuery(
-    "data",
-    () =>
-      HandleFetch(
-        "http://127.0.0.1:8000/api/admin/categories",
-        "GET",
-        null,
-        token
-      ),
-    {
-      retry: 1,
-      retryDelay: 500,
-      refetchOnWindowFocus: false,
-      onError: (e) => {
-      },
-      onSuccess: (data) => {
-        console.log(data)
-      },
-    }
-  );
+
   return (
     <ErrorBoundary
       FallbackComponent={ErrorHandlerModal}
       onReset={() => {
-        setInfoState({
-          ...infoState,
+        setCategoiesState({
+          ...categoiesState,
           error: null,
         });
       }}
     >
       <CategoriesList
-        infoState={infoState}
-        setInfoState={setInfoState}
+        categoiesState={categoiesState}
+        setCategoiesState={setCategoiesState}
         token={token}
       />
     </ErrorBoundary>
--- src/Admin/Categories/components/CategoriesList.js
+++ src/Admin/Categories/components/CategoriesList.js
@@ -3,37 +3,68 @@ import { AdminNavBar } from "../../../components/AdminNavBar";
 import { useQuery } from "react-query";
 import { HandleFetch } from "../../../components/HandleFetch";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 export default function CategoriesList(props) {
   const { t } = useTranslation();
 
+  const navigate = useNavigate();
+
+  function viewDetails(categoryKey) {
+    navigate("/admin/category/".categoryKey);
+  }
+
+  //Chce to raczej trzymacz w cache bo za każdym razem nie ma sensu tego pobierać 
+  //Więc pobieram useQuery i zapisuje do tego 
+
+  const { isLoading, error, data, isFetching, refetch } = useQuery(
+    "data",
+    () =>
+      HandleFetch(
+        "http://127.0.0.1:8000/api/admin/categories",
+        "GET",
+        null,
+        props.token
+      ),
+    {
+      retry: 1,
+      retryDelay: 500,
+      refetchOnWindowFocus: false,
+      onError: (e) => {
+      },
+      onSuccess: (data) => {
+        console.log(data)
+      },
+    }
+  );
   return(
     <div className="container-fluid main-container mt-3">
-    {/* <div className="card position-relative shadow p-3 mb-5">
+    <div className="card position-relative p-3 mb-5  bg-dark shadow">
 
-        {state.modalShow?<ModalAddSet setSetState={setSetState} state={state} token={count}/>:null}
+        {/* {state.modalShow?<ModalAddSet setSetState={setSetState} state={state} token={count}/>:null}
         {jsonModal && state.json?<JsonModal jsonModal={jsonModal} setJsonModal={setJsonModal} json={state.json} />:null}
         {errorModal && state.errors?<ErrorModal errorModal={errorModal} setErrorModal={setErrorModal} error={errorCode()} />:null}
         {editState.modalShow && editState?<EditSet editModal={editState} setEditModal={setEditState} setSetState={setSetState} setsState={state}/>:null}
-        
-        <NavPanel setSetState={setSetState} state={state}/>
+         */}
+                <AdminNavBar />
         
         <hr className="line"/>
 
         <div className="table-title">
-            <h2>{t('SetsTitle')}</h2>
+            {/* <h2>{t('SetsTitle')}</h2> */}
         </div>
         <div>
             <table className="table">
                 <thead>
                     <tr>
-                        {createTableTitles()}
+                        {/* {createTableTitles()} */}
                         <th scope="col"></th>
                         <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
-                    {createTable()}
+                    {/* {createTable()} */}
                 </tbody>
             </table>
         </div>
@@ -44,9 +75,9 @@ export default function CategoriesList(props) {
                     size="lg"
                     color="dark"
                     className=" btn button mt-2"
-                    onClick={()=>setSetState({...state,modalShow:(!state.modalShow)})}
+                    // onClick={()=>setSetState({...state,modalShow:(!state.modalShow)})}
                 >
-                    + {t('AddSetButton')}
+                    {/* + {t('AddSetButton')} */}
                 </Button>
             </div>
             <div className="col">
@@ -55,14 +86,14 @@ export default function CategoriesList(props) {
                     size="lg"
                     color="dark"
                     className=" btn button mt-2"
-                    onClick={()=>setJsonModal(!jsonModal)}
+                    // onClick={()=>setJsonModal(!jsonModal)}
                 >
-                    {t('SetJson')}
+                    {/* {t('SetJson')} */}
                 </Button>
             </div>
-            {(state.redirect && state.redirect !== undefined && state.redirectTo && state.redirectTo !== undefined)?<Redirect to={state.redirectTo} />:null}
+            {/* {(state.redirect && state.redirect !== undefined && state.redirectTo && state.redirectTo !== undefined)?<Redirect to={state.redirectTo} />:null} */}
         </div>
-    </div> */}
+    </div>
 </div>
     );
 }
