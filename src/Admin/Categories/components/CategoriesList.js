1bdf899153fb05ee4b0712771997cce186ea6280
damian.mosinski@vobacom.pl <mosinskidamian11@gmail.com>
2023-01-27 17:38:34 +0100

    Walka

--- src/Admin/Categories/components/CategoriesList.js
+++ src/Admin/Categories/components/CategoriesList.js
@@ -39,7 +39,10 @@ export default function CategoriesList(props) {
       refetchOnWindowFocus: false,
       onError: (e) => {},
       onSuccess: (data) => {
+        console.log(data.categories)
         if (dateUpdate < Date.now()) {
+          categoriesStore.removeCategories();
+
           for (const category of data.categories) {
             categoriesStore.addCategory(category);
           }
--- src/Admin/Categories/components/RenderList.js
+++ src/Admin/Categories/components/RenderList.js
@@ -1,20 +1,32 @@
+import React, { useEffect } from "react";
+
 export default function RenderList(props) {
   // https://codesandbox.io/s/13mxj2w2j7?file=/src/js/Account/TreeView.js
-  //   function recursiveTree(array) {
-  //     let ar = [];
-  //     for(const element of array) {
-  //         let child = [];
-  //         // console.log(element)
-  //         child["parent"] = element;
-  //         if(element["children"].length != 0){
-  //             let cos = recursiveTree(element["children"])
-  //             for(const [index, value] of cos.entries()){
-  //                 child["parent"]["child"] = value.name;
-  //             }
-  //         }
-  //         ar.push(child)
-  //     };
-  //     return ar;
-  //   }
-  return <div>cos</div>;
+    function recursiveTree(array) {
+      let elArray= [];
+      for(const [index, element] of array.entries()) {
+
+          let elementList = [];
+          if(element["children"].length != 0){
+            console.log("cs")
+              let cos = recursiveTree(element["children"])
+              for(const [index, value] of cos.entries()){
+                elementList[index] = React.createElement("li",{},elementList)
+                let childList = <ul></ul>
+                console.log(value)
+                // element.appendChild()child["parent"]["child"] = value;
+              }
+          }
+   
+        // let el = React.createElement("li",{},elementList);
+        // elArray[index] = el;
+      };
+      return elArray;
+    }
+
+
+  return (
+    
+  <div><ul>{recursiveTree(props.categories)}</ul>cos</div>
+  );
 }
