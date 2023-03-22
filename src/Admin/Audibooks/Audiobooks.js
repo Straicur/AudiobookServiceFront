17c7e744dd312c4c8f9842dd6f08dcb5dae74e6d
damian.mosinski@vobacom.pl <mosinskidamian11@gmail.com>
2023-03-22 16:23:04 +0100

    Podstawa listy audiobooków

--- src/Admin/Audibooks/Audiobooks.js
+++ src/Admin/Audibooks/Audiobooks.js
@@ -1,7 +1,31 @@
+import React, { useState } from "react";
+import { useTokenStore } from "../../store";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorHandlerModal } from "../../Errors/ErrorHandlerModal";
+import AudiobooksList from "./Components/AudiobooksList";
 
 export default function Audiobooks() {
-    return (
-      <div className="App">
-      </div>
-    );
+  const token = useTokenStore((state) => state.token);
+
+  const [audiobooksState, setAudiobooksState] = useState({
+    error: null,
+  });
+
+  return (
+    <ErrorBoundary
+      FallbackComponent={ErrorHandlerModal}
+      onReset={() => {
+        setAudiobooksState({
+          ...audiobooksState,
+          error: null,
+        });
+      }}
+    >
+      <AudiobooksList
+        audiobooksState={audiobooksState}
+        setAudiobooksState={setAudiobooksState}
+        token={token}
+      />
+    </ErrorBoundary>
+  );
 }
