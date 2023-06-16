bccbd9430a70b5a0f46e7e67e7adc340659baa3f
damian.mosinski@vobacom.pl <mosinskidamian11@gmail.com>
2023-06-16 18:28:12 +0200

    About

--- src/User/Policy/Policy.js
+++ src/User/Policy/Policy.js
@@ -1,16 +1,20 @@
 import { useTranslation } from "react-i18next";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Footer } from "../../Components/Footers/Footer";
+import "./Policy.css";
 
 export default function Policy() {
+  const { t } = useTranslation();
+
   return (
     <HelmetProvider>
       <Helmet>
         <style>{"body { background-color: black; }"}</style>
       </Helmet>
-
-      <div className="container-fluid main-container mt-3">
-        <div className="card position-relative p-3 bg-dark shadow"></div>
+      <div className="container-fluid main-container policy_page_container mt-3">
+        <div className="card position-relative p-3 bg-dark shadow policy_page text-center text-white">
+          <div className="row fs-2">{t("privacyPolicyDesc")}</div>
+        </div>
       </div>
       <Footer />
     </HelmetProvider>
