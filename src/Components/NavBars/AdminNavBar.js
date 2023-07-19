a5ac5416bc47a0bc8dedf58df22ed7c2067757e3
damian.mosinski@vobacom.pl <mosinskidamian11@gmail.com>
2023-07-19 20:22:19 +0200

    Poprawa wyglądu oraz fetch

--- src/Components/NavBars/AdminNavBar.js
+++ src/Components/NavBars/AdminNavBar.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import { useTranslation } from "react-i18next";
 import { HandleFetch } from "../HandleFetch";
 import { useTokenStore } from "../../store";
 import { useNavigate } from "react-router-dom";
+import { useNotificationsListStore } from "../../store";
 import Badge from "react-bootstrap/Badge";
 import "./AdminNavBar.css";
 
 export const AdminNavBar = () => {
   const { t, i18n } = useTranslation();
+
+  const [state, setState] = useState({
+    json: null,
+    page:0,
+    limit:10,
+    addCategoryModal: false,
+    refresh: false,
+    error: null,
+  });
 
   const tokenStore = useTokenStore();
 
@@ -27,6 +37,48 @@ export const AdminNavBar = () => {
       navigate("/login");
     });
   };
+  const notificationsListStore = useNotificationsListStore();
+
+  const notifications = useNotificationsListStore((state) => state.notifications);
+  const newNotifications = useNotificationsListStore((state) => state.newNotifications);
+  const dateUpdate = useNotificationsListStore((state) => state.dateUpdate);
+
+  const fetchCategories = () => {
+    HandleFetch(
+      "/notifications",
+      "POST",
+      {
+        page:state.page,
+        limit:state.limit,
+      },
+      token,
+      i18n.language
+    )
+      .then((data) => {
+        console.log(data)
+        // maxPage
+        // newNotifications
+        // page
+        // systemNotifications
+      })
+      .catch((e) => {
+        // props.setCategoiesState({
+        //   ...props.categoiesState,
+        //   error: e,
+        // });
+      });
+  };
+  const openNitificationList = () => {
+    console.log("as")
+  }
+
+  useEffect(() => {
+    setState({ ...state, json: notifications });
+
+    if (dateUpdate < Date.now() || state.refresh) {
+      fetchCategories();
+    }
+  }, []);
 
   return (
     <div className="row navbar">
@@ -114,7 +166,13 @@ export const AdminNavBar = () => {
           </Button>
         </ButtonGroup>
         {/* Cały ten element ma być clicable i dodaj mu hover na jaśniejszy
-            Pobieranie noramlnie Fetchem i zapisuje to do pamięci z odświerzaniem 4 minuty
+            Pobieranie noramlnie Fetchem i zapisuje to do pamięci z odświerzaniem co 5 minut
+            Najpierw sprawdzam czy coś tam jest i jeśli nie to pobieram 
+            Jeśli tak to sprawdzam czas czy jest mniejszy i jak tak to pobieram
+            Ogólnie ten czas update robie przy pracy z powiadomieniami żeby mi to za bardzo się nie wydłużało i żeby nie było za kótkie
+            I Zmieniam przy okazji tą ilośc odczytanych w zależności od tamtych detali
+
+
             Pobieram ilość z pamięci i po kliknięciu pobieram pierwszą stronę, na końcu listy napis "Pokaż więcej"
             I po tym ładuję więcej i tyle, znikać ma po dojściu do max 
             Pomyśl też nad przeczytaniem tego powiadomienia bo to będzie mi pokazywać ilość wszystkich a powinno tylko 
@@ -122,13 +180,13 @@ export const AdminNavBar = () => {
 
             Po kliknięciu w powiadomienia pokazuje się modal który wyświetli detale i nie wyłącza mi się ta lista wogóle jak coś.
         */}
-        <div className="row pt-2 me-1 h-100 align-items-center justify-content-center">
-          <div className="col justify-content-end  align-items-center">
+        <div className="row mx-1 pt-3 ms-1 me-3 align-items-center justify-content-center notification-row" onClick={openNitificationList}>
+          <div className="col nav-col justify-content-end  align-items-center pe-2">
             <h6> {t("notifications")}</h6>
           </div>
-          <div className="col justify-content-end  align-items-center">
+          <div className="col nav-col justify-content-end  align-items-center">
             <h6>
-              <Badge bg="secondary">New</Badge>
+              <Badge bg="secondary">{newNotifications}</Badge>
             </h6>
           </div>
         </div>
