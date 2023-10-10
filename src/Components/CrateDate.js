19205975b5016ac44aba6ee6e9ffd20aa1450509
damian.mosinski@vobacom.pl <mosinskidamian11@gmail.com>
2023-10-10 21:14:13 +0200

    Poprawa wyświetlanej daty użytkownika

--- src/Components/CrateDate.js
+++ src/Components/CrateDate.js
@@ -2,11 +2,11 @@ export const CreateDate = (timeStamp) => {
   const dateFormat = new Date(timeStamp);
 
   const day =
-    dateFormat.getDate() < 10
+    dateFormat.getDate() < 9
       ? "0" + dateFormat.getDate()
       : dateFormat.getDate();
   const month =
-    dateFormat.getMonth() < 10
+    dateFormat.getMonth() < 9
       ? "0" + (dateFormat.getMonth() + 1)
       : dateFormat.getMonth() + 1;
   const year = dateFormat.getFullYear();
