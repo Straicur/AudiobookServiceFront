2fb15094f507f46197000d2e0fdff01694b418d2
damian.mosinski@vobacom.pl <mosinskidamian11@gmail.com>
2023-10-14 10:57:39 +0200

    Poprawki rerenderu playera

--- src/Admin/Audibooks/Components/Audiobook/AudiobookPlayer.js
+++ src/Admin/Audibooks/Components/Audiobook/AudiobookPlayer.js
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 
 export default function AudiobookPlayer(props) {
+  const player = useRef(null);
+  const timeAudio = useRef(0);
+
   const nextPart = () => {
     let nextPart = props.audiobookState.part + 1;
 
     if (nextPart < props.audiobookDetail.parts) {
+      timeAudio.current = 0;
+      player.current = null;
       props.setAudiobookState({ ...props.audiobookState, part: nextPart });
     }
   };
+
+  const timeCur = (audio) => {
+    timeAudio.current = parseInt(audio.target.currentTime);
+  };
+
   const prevPart = () => {
     let prevPart = props.audiobookState.part - 1;
 
     if (prevPart >= 0) {
+      timeAudio.current = 0;
+      player.current = null;
       props.setAudiobookState({ ...props.audiobookState, part: prevPart });
     }
   };
-  //todo tu musze ustawiać state ale to w jakiejś stałej i jeśli mi odświerzy to ustawiać go i autoPlay na true 
-  // Przy zmianie parta powinno się zerować (W tych metodach w ifach na 0 to ustawić)
-  // To będzie trzymać tylko przy rerenderze więc to może zostać 
+ 
+  useEffect(() => {
+    if (player.current && timeAudio.current) {
+      player.current.audio.current.currentTime = timeAudio.current;
+    }
+  }, [props]);
+ 
   return (
     <AudioPlayer
       header={
@@ -31,10 +47,12 @@ export default function AudiobookPlayer(props) {
       }
       autoPlay={false}
       src={window.URL.createObjectURL(new Blob([props.audiobookPart]))}
+      onListen={(e) => timeCur(e)}
       autoPlayAfterSrcChange={false}
       showSkipControls={true}
       onClickPrevious={prevPart}
       onClickNext={nextPart}
+      ref={player}
     />
   );
 }
