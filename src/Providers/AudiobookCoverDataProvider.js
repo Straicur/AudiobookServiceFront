ac6298ae6de1dda171a04ee9a451b9d6740828c4
Straicur <mosinskidamian11@gmail.com>
2024-01-27 12:29:38 +0100

    feat: Zmiana rozszerzeń na jsx

--- src/Providers/AudiobookCoverDataProvider.js
+++ src/Providers/AudiobookCoverDataProvider.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect } from "react";
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import { useQuery } from "react-query";
 import { HandleFetch } from "../Util/HandleFetch";
 
