9fb6f467695c69f1f0e9c4b3d37960b2d00561f7
mosinskidamian11@gmail.com <mosinskidamian11@gmail.com>
2024-06-28 17:59:49 +0200

    feat: Dalsza praca

--- src/Providers/User/UserAudiobookInfoProvider.js
+++ src/Providers/User/UserAudiobookInfoProvider.js
@@ -11,6 +11,14 @@ export const UserAudiobookInfoProvider = ({ children, token, audiobookId, catego
 
   const { mutate } = useMutation({
     mutationFn: (data) => {
+      console.log({
+        audiobookId: data.props.state.detailModalAudiobook.id,
+        categoryKey: data.props.state.detailModalCategory.categoryKey,
+        part: data.audiobookInfoPartToSave.current + 1,
+        endedTime: data.timeAudio.current,
+        watched: data.watched,
+      });
+
       return HandleFetch(
         '/user/audiobook/info/add',
         'PUT',
