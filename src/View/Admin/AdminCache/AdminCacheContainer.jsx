d3f7c358c1921d9e7ef9c19f7555d195dbe7c113
mosinskidamian11@gmail.com <mosinskidamian11@gmail.com>
2024-06-29 11:29:57 +0200

    feat: Poprawa Playera

--- src/View/Admin/AdminCache/AdminCacheContainer.jsx
+++ src/View/Admin/AdminCache/AdminCacheContainer.jsx
@@ -2,18 +2,28 @@ import React from 'react';
 import AdminNavBarProviders from '../AdminNavBar/AdminNavBarProviders';
 import { Button } from 'react-bootstrap';
 import { useAdminCacheContextData } from 'Providers/Admin/AdminCacheProvider';
+import { useUserAudiobooksListStore } from 'Store/store';
+import { useNotificationsListStore } from 'Store/store';
 
 export default function AdminCacheContainer(props) {
   const [pools, clearCache] = useAdminCacheContextData();
+  const audiobooksListStore = useUserAudiobooksListStore();
+  const notificationsListStore = useNotificationsListStore();
 
   console.log(pools);
+
+  const clearAppCache = () => {
+    clearCache({ all: true });
+    notificationsListStore.removeNotifications();
+    audiobooksListStore.removeAudiobooks();
+  };
 
   return (
     <div className='container-fluid main-container mt-3'>
       <div className='card position-relative p-3 mb-5  shadow'>
         <AdminNavBarProviders token={props.token} t={props.t} i18n={props.i18n} />
         <hr className='line' />
-        <Button onClick={() => clearCache({ all: true })}>{props.t('clearCachePools')}</Button>
+        <Button onClick={() => clearAppCache()}>{props.t('clearCachePools')}</Button>
         <div className='table-title my-2'>
           <h1>{props.t('cachePools')}</h1>
         </div>
